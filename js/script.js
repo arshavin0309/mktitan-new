079e21f429cd8c4b450ac6f2c070aee921c1ccda
Иванов Владислав <arshavin0309@mail.ru>
2023-11-24 21:27:59 +0500

    complete

--- js/script.js
+++ js/script.js
@@ -12,43 +12,43 @@ window.addEventListener('DOMContentLoaded', () => {
         slidesPerView: 3,
       }
     },
-  
+
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
-  
+
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
+
     // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
   });
 
-  
+
   const swiperProduct = new Swiper('.swiper-product', {
     // Optional parameters
     loop: true,
     slidesPerView: 1,
-  
+
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
-  
+
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
+
     // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
@@ -59,16 +59,29 @@ window.addEventListener('DOMContentLoaded', () => {
 // Функция ymaps.ready() будет вызвана, когда
 // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
 ymaps.ready(init);
-function init(){
-    // Создание карты.
-    var myMap = new ymaps.Map("map", {
-        // Координаты центра карты.
-        // Порядок по умолчанию: «широта, долгота».
-        // Чтобы не определять координаты центра карты вручную,
-        // воспользуйтесь инструментом Определение координат.
-        center: [55.76, 37.64],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
-        zoom: 7
-    });
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [55.76, 37.64],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 7
+  });
+
+
+
 }
+
+document.querySelector('.header-burger').addEventListener('click', ()=>{
+  document.querySelector('.menu-in-mobile').classList.toggle('menu-in-mobile--active')
+  console.log('1121232')
+})
+
+document.querySelector('.catalog__show-filter').addEventListener('click', ()=>{
+  document.querySelector('.catalog__filter').classList.toggle('block')
+  console.log('1121232')
+})
